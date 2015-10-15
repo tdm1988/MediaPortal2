ff1f8dd5626217371a8b9f0de700e07f0c8153d4
henso96 <henso_96@hotmail.com>
2015-10-15 18:15:29 +0200

    Added transcode progress support

--- MediaPortal/Incubator/TranscodingService/Transcoders/Base/TranscodeContext.cs
+++ MediaPortal/Incubator/TranscodingService/Transcoders/Base/TranscodeContext.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MediaPortal.Common;
 using MediaPortal.Common.Logging;
+using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace MediaPortal.Plugins.Transcoding.Service.Transcoders.Base
 {
@@ -14,14 +16,67 @@ namespace MediaPortal.Plugins.Transcoding.Service.Transcoders.Base
   {
     StringBuilder _errorOutput = new StringBuilder();
     StringBuilder _standardOutput = new StringBuilder();
+    Regex progressRegex = new Regex(@"frame=\s*(?<frame>[0-9]*)\s*fps=(?<fps>[0-9]*)\s*q=(?<quality>[0-9|\.]*)\s*size=\s*(?<size>\S*)\s*time=(?<time>.*)\s*bitrate=(?<bitrate>\S*)", RegexOptions.IgnoreCase);
 
-    private Stream _transcodedStream;
+    Stream _transcodedStream;
+    long _lastSize = 0;
+    TimeSpan _lastTime = TimeSpan.FromTicks(0);
+    object _lastSync = new object();
 
     public string TargetFile { get; internal set; }
     public string SegmentDir { get; internal set; }
     public string HlsBaseUrl { get; internal set; }
     public bool Aborted { get; internal set; }
     public bool Failed { get; internal set; }
+    public bool Partial { get; internal set; }
+    public TimeSpan TargetDuration { get; internal set; }
+    public TimeSpan CurrentDuration 
+    { 
+      get
+      {
+        return _lastTime;
+      }
+    }
+    public long TargetFileSize 
+    { 
+      get
+      {
+        if (Running)
+        {
+          if (_lastSize > 0 && Partial == false)
+          {
+            lock (_lastSync)
+            {
+              double secondSize = Convert.ToDouble(_lastSize) / _lastTime.TotalSeconds;
+              return Convert.ToInt64(secondSize * TargetDuration.TotalSeconds);
+            }
+          }
+          return 0;
+        }
+        else
+        {
+          if (_transcodedStream != null)
+          {
+            return _transcodedStream.Length;
+          }
+        }
+        return 0;
+      }
+    }
+    public long CurrentFileSize
+    {
+      get
+      {
+        if (_transcodedStream != null)
+        {
+          return _transcodedStream.Length;
+        }
+        else
+        {
+          return _lastSize;
+        }
+      }
+    }
     public string ConsoleErrorOutput
     {
       get
@@ -63,12 +118,35 @@ namespace MediaPortal.Plugins.Transcoding.Service.Transcoders.Base
 
     internal void ErrorDataReceived(object sender, DataReceivedEventArgs e)
     {
-      _errorOutput.Append(e.Data);
+      //frame= 4152 fps=115 q=30.0 size=   40610kB time=00:02:53.56 bitrate=1916.8kbits/s  
+      if (string.IsNullOrEmpty(e.Data) == false)
+      {
+        Match match = progressRegex.Match(e.Data);
+        if (match.Success)
+        {
+          lock (_lastSync)
+          {
+            if (match.Groups["size"].Value.EndsWith("kB", StringComparison.InvariantCultureIgnoreCase))
+            {
+              _lastSize = long.Parse(match.Groups["size"].Value.Substring(0, match.Groups["size"].Value.Length - 2).Trim()) * 1024;
+            }
+            else if (match.Groups["size"].Value.EndsWith("mB", StringComparison.InvariantCultureIgnoreCase))
+            {
+              _lastSize = long.Parse(match.Groups["size"].Value.Substring(0, match.Groups["size"].Value.Length - 2).Trim()) * 1024 * 1024;
+            }
+            _lastTime = TimeSpan.Parse(match.Groups["time"].Value);
+          }
+        }
+        _errorOutput.Append(e.Data);
+      }
     }
 
     internal void OutputDataReceived(object sender, DataReceivedEventArgs e)
     {
-      _standardOutput.Append(e.Data);
+      if (string.IsNullOrEmpty(e.Data) == false)
+      {
+        _standardOutput.Append(e.Data);
+      }
     }
 
     public void Start()
@@ -87,6 +165,48 @@ namespace MediaPortal.Plugins.Transcoding.Service.Transcoders.Base
     public void Stop()
     {
       Running = false;
+    }
+
+    public void DeleteFiles()
+    {
+      string deletePath = TargetFile;
+      bool isFolder = false;
+      if (deletePath.EndsWith(".m3u8") == true)
+      {
+        deletePath = SegmentDir;
+        isFolder = true;
+      }
+
+      DateTime start = DateTime.Now;
+      while ((DateTime.Now - start).TotalSeconds < 5.0)
+      {
+        try
+        {
+          if (isFolder == false)
+          {
+            if (File.Exists(deletePath))
+            {
+              File.Delete(deletePath);
+            }
+            else
+            {
+              break;
+            }
+          }
+          else
+          {
+            if (Directory.Exists(deletePath))
+            {
+              Directory.Delete(deletePath, true);
+            }
+            else
+            {
+              break;
+            }
+          }
+        }
+        catch { }
+      }
     }
 
     public void Dispose()
