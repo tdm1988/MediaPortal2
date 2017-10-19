97fa6c5fe7c99f0dd2db90982c2b61c83afb9469
aspik <aspik@team-mediaportal.com>
2017-10-19 23:38:53 +0200

    MP2-662: first version of reworked audio model

--- MediaPortal/Source/UI/UiComponents/Media/Models/AudioPlayerModel.cs
+++ MediaPortal/Source/UI/UiComponents/Media/Models/AudioPlayerModel.cs
@@ -23,8 +23,12 @@
 #endregion
 
 using System;
+using MediaPortal.Common;
+using MediaPortal.Common.General;
+using MediaPortal.Common.MediaManagement;
 using MediaPortal.UI.Presentation.Players;
 using MediaPortal.UiComponents.Media.General;
+using MediaPortal.UI.Control.InputManager;
 
 namespace MediaPortal.UiComponents.Media.Models
 {
@@ -35,9 +39,16 @@ namespace MediaPortal.UiComponents.Media.Models
   {
     public const string MODEL_ID_STR = "D8998340-DA2D-42be-A29B-6D7A72AEA2DC";
     public static readonly Guid MODEL_ID = new Guid(MODEL_ID_STR);
+    protected AbstractProperty _mediaItemProperty;
+    protected AbstractProperty _isPlayerControlVisibleProperty;
+    protected MediaItem _currentMediaItem;
+    private bool _isPlayerCtrlOpenOnDemand;
+    private DateTime _lastPlayerCtrlDemand = DateTime.MinValue;
 
     public AudioPlayerModel() : base(Consts.WF_STATE_ID_CURRENTLY_PLAYING_AUDIO, Consts.WF_STATE_ID_FULLSCREEN_AUDIO)
     {
+      _mediaItemProperty = new SProperty(typeof(MediaItem), null);
+      _isPlayerControlVisibleProperty = new WProperty(typeof(bool), false);
     }
 
     protected override Type GetPlayerUIContributorType(IPlayer player, MediaWorkflowStateType stateType)
@@ -57,10 +68,59 @@ namespace MediaPortal.UiComponents.Media.Models
     protected override void Update()
     {
       // base.Update is abstract
-      // Nothing to do here
+      IPlayerContextManager playerContextManager = ServiceRegistration.Get<IPlayerContextManager>();
+      IPlayerContext playerContext = playerContextManager.CurrentPlayerContext;
+      _currentMediaItem = playerContext == null ? null : playerContext.CurrentMediaItem;
+      IInputManager inputManager = ServiceRegistration.Get<IInputManager>();
+
+      if (!_isPlayerCtrlOpenOnDemand)
+      {
+        if (DateTime.Now - _lastPlayerCtrlDemand > DateTime.Now.AddSeconds(5) - DateTime.Now)
+        {
+          IsPlayerControlVisible = inputManager.IsMouseUsed;
+        }
+      }
+
+      MediaItem = _currentMediaItem;
     }
 
     #region Members to be accessed from the GUI
+
+    public void TogglePlayerControl()
+    {
+      IsPlayerControlVisible = !IsPlayerControlVisible;
+      _isPlayerCtrlOpenOnDemand = IsPlayerControlVisible;
+      _lastPlayerCtrlDemand = DateTime.Now;
+    }
+
+    public void ClosePlayerControl()
+    {
+      IsPlayerControlVisible = false;
+      _lastPlayerCtrlDemand = DateTime.Now;
+      _isPlayerCtrlOpenOnDemand = false;
+    }
+
+    public AbstractProperty MediaItemProperty
+    {
+      get { return _mediaItemProperty; }
+    }
+
+    public MediaItem MediaItem
+    {
+      get { return (MediaItem)_mediaItemProperty.GetValue(); }
+      internal set { _mediaItemProperty.SetValue(value); }
+    }
+
+    public AbstractProperty IsPlayerControlVisibleProperty
+    {
+      get { return _isPlayerControlVisibleProperty; }
+    }
+
+    public bool IsPlayerControlVisible
+    {
+      get { return (bool)_isPlayerControlVisibleProperty.GetValue(); }
+      set { _isPlayerControlVisibleProperty.SetValue(value); }
+    }
 
     #endregion
 
