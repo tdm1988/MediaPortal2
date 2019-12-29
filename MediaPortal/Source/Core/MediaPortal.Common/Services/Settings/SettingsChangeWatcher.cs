b147645b08f510a7ec87dbe050f52565f02c4193
henso96 <henso_96@hotmail.com>
2019-12-29 18:19:51 +0100

    Changed settings watcher to be single threaded and added unit tests for comparison

--- MediaPortal/Source/Core/MediaPortal.Common/Services/Settings/SettingsChangeWatcher.cs
+++ MediaPortal/Source/Core/MediaPortal.Common/Services/Settings/SettingsChangeWatcher.cs
@@ -24,13 +24,124 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using MediaPortal.Common.Logging;
 using MediaPortal.Common.Messaging;
 using MediaPortal.Common.Settings;
 using MediaPortal.Common.UserManagement;
 
 namespace MediaPortal.Common.Services.Settings
 {
+  public static class SettingsChangeWatcher
+  {
+    private const int MAX_CONCURRENT_CALLS = 1;
+    private const int LONG_RUNNING_WARNING_THRESHOLD_MSEC = 30000;
+
+    private static readonly object SYNC_OBJECT = new object();
+    private static readonly List<InvocationHandler> INVOCATION_LIST = new List<InvocationHandler>();
+    private static readonly TaskFactory INVOCATION_FACTORY = new TaskFactory(CancellationToken.None, TaskCreationOptions.LongRunning, TaskContinuationOptions.DenyChildAttach, System.Threading.Tasks.TaskScheduler.Default);
+
+    private static AsynchronousMessageQueue _messageQueue;
+    private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+    public static bool IsRunning => _messageQueue != null;
+    public static int InvocationCount => INVOCATION_LIST.Count;
+
+    private class InvocationHandler
+    {
+      public InvocationHandler(MessageReceivedHandler handler)
+      {
+        Handler = handler;
+      }
+      public SemaphoreSlim HandlerReady { get; } = new SemaphoreSlim(MAX_CONCURRENT_CALLS, MAX_CONCURRENT_CALLS);
+      public MessageReceivedHandler Handler { get; }
+    }
+
+    internal static event MessageReceivedHandler MessageReceived
+    {
+      add
+      {
+        lock (SYNC_OBJECT)
+        {
+          StartSettingWatcher();
+          INVOCATION_LIST.Add(new InvocationHandler(value));
+        }
+      }
+      remove
+      {
+        lock (SYNC_OBJECT)
+        {
+          var first = INVOCATION_LIST.FirstOrDefault(i => i.Handler == value);
+          if (first != null)
+            INVOCATION_LIST.Remove(first);
+          if (INVOCATION_LIST.Count == 0)
+            StopSettingWatcher();
+        }
+      }
+    }
+
+    private static void OnMessageReceived(AsynchronousMessageQueue queue, SystemMessage message)
+    {
+      if (message.ChannelName == UserMessaging.CHANNEL || message.ChannelName == SettingsManagerMessaging.CHANNEL)
+      {
+        lock (SYNC_OBJECT)
+        {
+          foreach (var invocation in INVOCATION_LIST)
+          {
+            INVOCATION_FACTORY.StartNew(() =>
+            {
+              ProcessInvocation(invocation, queue, message);
+            }, _cancellationTokenSource.Token);
+          }
+        }
+      }
+    }
+
+    private static async void ProcessInvocation(InvocationHandler invocation, AsynchronousMessageQueue queue, SystemMessage message)
+    {
+      if (MAX_CONCURRENT_CALLS == 0 || await invocation.HandlerReady.WaitAsync(LONG_RUNNING_WARNING_THRESHOLD_MSEC, _cancellationTokenSource.Token))
+      {
+        try
+        {
+          invocation.Handler.Invoke(queue, message);
+        }
+        catch (Exception e)
+        {
+          ServiceRegistration.Get<ILogger>().Error("Unhandled exception in message SettingChangedWatcher handler when handling a message of type '{0}'",
+              e, message.MessageType);
+        }
+        if (MAX_CONCURRENT_CALLS > 0)
+          invocation.HandlerReady.Release();
+      }
+      else if (LONG_RUNNING_WARNING_THRESHOLD_MSEC > 0)
+      {
+        ServiceRegistration.Get<ILogger>().Warn("Event handler running for more than {1} milliseconds detected in SettingChangedWatcher for messages of type '{0}'",
+          message.MessageType, LONG_RUNNING_WARNING_THRESHOLD_MSEC);
+      }
+    }
+
+    private static void StartSettingWatcher()
+    {
+      if (_messageQueue == null)
+      {
+        _cancellationTokenSource = new CancellationTokenSource();
+        _messageQueue = new AsynchronousMessageQueue(nameof(SettingsChangeWatcher), new[] { SettingsManagerMessaging.CHANNEL, UserMessaging.CHANNEL });
+        _messageQueue.MessageReceived += OnMessageReceived;
+        _messageQueue.Start();
+      }
+    }
+
+    private static void StopSettingWatcher()
+    {
+      _cancellationTokenSource.Cancel();
+      _messageQueue?.Shutdown();
+      _messageQueue?.Dispose();
+      _messageQueue = null;
+    }
+  }
+
   /// <summary>
   /// <see cref="SettingsChangeWatcher{T}"/> provides a generic watcher for settings that automatically refreshes
   /// the <see cref="Settings"/> if it gets notified using <see cref="SettingsManagerMessaging"/>.
@@ -41,8 +152,10 @@ namespace MediaPortal.Common.Services.Settings
   {
     #region Fields
 
-    protected AsynchronousMessageQueue _messageQueue;
     protected T _settings;
+
+    private readonly bool _updateOnUserChange;
+    private readonly object _syncObject = new object();
 
     #endregion
 
@@ -50,6 +163,102 @@ namespace MediaPortal.Common.Services.Settings
       : this(false) { }
 
     public SettingsChangeWatcher(bool updateOnUserChange)
+    {
+      _updateOnUserChange = updateOnUserChange;
+
+      SettingsChangeWatcher.MessageReceived += OnMessageReceived;
+    }
+
+    /// <summary>
+    /// Informs listeners that the current setting has been changed.
+    /// </summary>
+    public EventHandler SettingsChanged;
+
+    /// <summary>
+    /// Gets the current setting. This property will automatically return new values after changes.
+    /// </summary>
+    public T Settings
+    {
+      get
+      {
+        return _settings ?? (_settings = ServiceRegistration.Get<ISettingsManager>().Load<T>());
+      }
+    }
+
+    /// <summary>
+    /// Refreshes the settings asynchronously.
+    /// </summary>
+    public void RefreshAsync()
+    {
+      Task.Run(() => Refresh());
+    }
+
+    /// <summary>
+    /// Forces the refresh of the settings.
+    /// </summary>
+    public void Refresh()
+    {
+      _settings = null;
+      if (SettingsChanged != null)
+        SettingsChanged(this, EventArgs.Empty);
+    }
+
+    #region Message handling
+
+    protected void OnMessageReceived(AsynchronousMessageQueue queue, SystemMessage message)
+    {
+      if (_updateOnUserChange && message.ChannelName == UserMessaging.CHANNEL)
+      {
+        UserMessaging.MessageType messageType = (UserMessaging.MessageType)message.MessageType;
+        switch (messageType)
+        {
+          // If the user has been changed, refresh the settings in every case.
+          case UserMessaging.MessageType.UserChanged:
+            RefreshAsync();
+            break;
+        }
+      }
+      if (message.ChannelName == SettingsManagerMessaging.CHANNEL)
+      {
+        SettingsManagerMessaging.MessageType messageType = (SettingsManagerMessaging.MessageType)message.MessageType;
+        switch (messageType)
+        {
+          case SettingsManagerMessaging.MessageType.SettingsChanged:
+            Type settingsType = (Type)message.MessageData[SettingsManagerMessaging.SETTINGSTYPE];
+            // If our contained Type has been changed, clear the cache and reload it
+            if (typeof(T) == settingsType)
+              Refresh();
+            break;
+        }
+      }
+    }
+
+    #endregion
+
+    #region IDisposable members
+
+    public void Dispose()
+    {
+      SettingsChangeWatcher.MessageReceived -= OnMessageReceived;
+    }
+
+    #endregion
+  }
+
+  public class SettingsChangeWatcherOld<T> : IDisposable
+    where T : class
+  {
+    #region Fields
+
+    protected AsynchronousMessageQueue _messageQueue;
+    protected T _settings;
+
+    #endregion
+
+    public SettingsChangeWatcherOld()
+      : this(false) { }
+
+    public SettingsChangeWatcherOld(bool updateOnUserChange)
     {
       List<string> channels = new List<string> { SettingsManagerMessaging.CHANNEL };
       if (updateOnUserChange)
---
+++ MediaPortal/Tests/Tests.Server/Utilities/TestSettingsWatcher.cs
@@ -0,0 +1,532 @@
+﻿#region Copyright (C) 2007-2018 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2018 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using MediaPortal.Common.Messaging;
+using MediaPortal.Common.Services.Logging;
+using MediaPortal.Common.Services.Messaging;
+using MediaPortal.Common.Services.Settings;
+using MediaPortal.Common.Settings;
+using MediaPortal.Utilities.Cache;
+using NUnit.Framework;
+
+namespace Tests.Server.Utilities
+{
+  public class TestSettingsManager : ISettingsManager
+  {
+    #region ISettingsManager Members
+
+    public SettingsType Load<SettingsType>() where SettingsType : class
+    {
+      return (SettingsType)Load(typeof(SettingsType));
+    }
+
+    public object Load(Type settingsType)
+    {
+      return Activator.CreateInstance(settingsType);
+    }
+
+    public void Save(object settingsObject)
+    {
+      // Send a message that the setting has been changed.
+      Type t = settingsObject.GetType();
+      SettingsManagerMessaging.SendSettingsChangeMessage(t);
+    }
+
+    public void StartBatchUpdate() { }
+
+    public void EndBatchUpdate() { }
+
+    public void CancelBatchUpdate() { }
+
+    public void ClearCache() { }
+
+    public void ChangeUserContext(string userName) { }
+
+    public void RemoveSettingsData(Type settingsType, bool user, bool global) { }
+
+    public void RemoveAllSettingsData(bool user, bool global) { }
+
+    #endregion
+  }
+
+  public class TestSetting
+  {
+    [Setting(SettingScope.Global, true)]
+    public bool Test { get; set; }
+  }
+
+  [TestFixture]
+  public class TestSettingsWatcher
+  {
+    private const int WATCHER_COUNT = 300;
+    private const int WAIT_TIMEOUT = 1000;
+    private const int MANY_FACTOR = 10;
+
+    private static SemaphoreSlim _testSync = new SemaphoreSlim(1, 1);
+
+    [OneTimeSetUp]
+    public void Init()
+    {
+      ServiceRegistration.Set<IMessageBroker>(new MessageBroker());
+      ServiceRegistration.Set<ISettingsManager>(new TestSettingsManager());
+      ServiceRegistration.Set<ILogger>(new NoLogger());
+    }
+
+    private List<SettingsChangeWatcher<TestSetting>> CreateWatchers(int count, EventHandler eventHandler, bool shareEvent)
+    {
+      List<SettingsChangeWatcher<TestSetting>> watchers = new List<SettingsChangeWatcher<TestSetting>>();
+      for (int i = 0; i < count; i++)
+      {
+        var watcher = new SettingsChangeWatcher<TestSetting>();
+        if (!shareEvent)
+        {
+          EventHandler evt = (s, e) =>
+          {
+            eventHandler.Invoke(s, e);
+          };
+          watcher.SettingsChanged += evt;
+        }
+        else
+        {
+          watcher.SettingsChanged += eventHandler;
+        }
+        watchers.Add(watcher);
+      }
+      return watchers;
+    }
+
+    private void DisposeWatchers(List<SettingsChangeWatcher<TestSetting>> watchers, bool skipFirst = false)
+    {
+      for (int i = 0; i < watchers.Count; i++)
+      {
+        if (i == 0 && skipFirst)
+          continue;
+
+        watchers[i].Dispose();
+      }
+    }
+
+    private List<SettingsChangeWatcherOld<TestSetting>> CreateOldWatchers(int count, EventHandler eventHandler, bool shareEvent)
+    {
+      List<SettingsChangeWatcherOld<TestSetting>> watchers = new List<SettingsChangeWatcherOld<TestSetting>>();
+      for (int i = 0; i < count; i++)
+      {
+        var watcher = new SettingsChangeWatcherOld<TestSetting>();
+        if (!shareEvent)
+        {
+          EventHandler evt = (s, e) =>
+          {
+            eventHandler.Invoke(s, e);
+          };
+          watcher.SettingsChanged += evt;
+        }
+        else
+        {
+          watcher.SettingsChanged += eventHandler;
+        }
+        watchers.Add(watcher);
+      }
+      return watchers;
+    }
+
+    private void DisposeOldWatchers(List<SettingsChangeWatcherOld<TestSetting>> watchers, bool skipFirst = false)
+    {
+      for (int i = 0; i < watchers.Count; i++)
+      {
+        if (i == 0 && skipFirst)
+          continue;
+
+        watchers[i].Dispose();
+      }
+    }
+
+    private void WaitForEvents(int timeout, Func<bool> isDone)
+    {
+      int waitCount = timeout / 100;
+      while (!isDone() && waitCount > 0)
+      {
+        waitCount--;
+        Thread.Sleep(100);
+      }
+    }
+
+    private void FireEvents()
+    {
+      var settings = ServiceRegistration.Get<ISettingsManager>().Load<TestSetting>();
+      ServiceRegistration.Get<ISettingsManager>().Save(settings);
+    }
+
+    [Test, Order(1)]
+    public void TestSettingChangeStartStop()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        EventHandler evt = (s, e) => { };
+        var watchers = CreateWatchers(WATCHER_COUNT, evt, false);
+
+        //Act
+        bool started = SettingsChangeWatcher.IsRunning;
+        DisposeWatchers(watchers, true);
+        bool stillStarted = SettingsChangeWatcher.IsRunning;
+        watchers[0].Dispose();
+
+        //Assert
+        Assert.IsTrue(started, "SettingsChangeWatcher never started");
+        Assert.IsTrue(stillStarted, "SettingsChangeWatcher stopped too early");
+        Assert.IsTrue(!SettingsChangeWatcher.IsRunning, "SettingsChangeWatcher still running");
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(2)]
+    public void TestSettingChangeStartStopSharedEvent()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        EventHandler evt = (s, e) => { };
+        var watchers = CreateWatchers(WATCHER_COUNT, evt, true);
+
+        //Act
+        bool started = SettingsChangeWatcher.IsRunning;
+        DisposeWatchers(watchers, true);
+        bool stillStarted = SettingsChangeWatcher.IsRunning;
+        watchers[0].Dispose();
+
+        //Assert
+        Assert.IsTrue(started, "SettingsChangeWatcher never started");
+        Assert.IsTrue(stillStarted, "SettingsChangeWatcher stopped too early");
+        Assert.IsTrue(!SettingsChangeWatcher.IsRunning, "SettingsChangeWatcher still running");
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(3)]
+    public void TestSettingChangeLongRunningAbort()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int expectedCount = WATCHER_COUNT;
+        EventHandler evt = (s, e) =>
+        {
+          var count = Interlocked.Increment(ref eventTriggerCount);
+          Thread.Sleep(WAIT_TIMEOUT * 2);
+        };
+        var watchers = CreateWatchers(expectedCount, evt, false);
+
+        //Act
+        bool started = SettingsChangeWatcher.IsRunning;
+        FireEvents();
+        WaitForEvents(WAIT_TIMEOUT, () => eventTriggerCount >= expectedCount);
+        DisposeWatchers(watchers);
+
+        //Assert
+        Assert.IsTrue(started, "SettingsChangeWatcher never started");
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+        Assert.IsTrue(!SettingsChangeWatcher.IsRunning, "SettingsChangeWatcher still running");
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(4)]
+    public void TestSettingChangeEvents()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int expectedCount = WATCHER_COUNT;
+        EventHandler evt = (s, e) =>
+        {
+          Interlocked.Increment(ref eventTriggerCount);
+        };
+        var watchers = CreateWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        WaitForEvents(WAIT_TIMEOUT, () => eventTriggerCount >= WATCHER_COUNT);
+
+        //Assert
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(5)]
+    public void TestSettingChangeEventsMany()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int expectedCount = WATCHER_COUNT * MANY_FACTOR;
+        EventHandler evt = (s, e) =>
+        {
+          Interlocked.Increment(ref eventTriggerCount);
+        };
+        var watchers = CreateWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        WaitForEvents(WAIT_TIMEOUT * MANY_FACTOR, () => eventTriggerCount >= WATCHER_COUNT * MANY_FACTOR);
+
+        //Assert
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(6)]
+    public void TestSettingChangeException()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int exceptionCount = 0;
+        int expectedCount = WATCHER_COUNT;
+        EventHandler evt = (s, e) =>
+        {
+          var count = Interlocked.Increment(ref eventTriggerCount);
+          if (count == 1 || count == WATCHER_COUNT / 2)
+          {
+            Interlocked.Increment(ref exceptionCount);
+            throw new Exception("Test");
+          }
+        };
+        var watchers = CreateWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        WaitForEvents(WAIT_TIMEOUT, () => eventTriggerCount >= WATCHER_COUNT);
+
+        //Assert
+        Assert.AreEqual(exceptionCount, 2);
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(7)]
+    public void TestSettingChangeLongRunning()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int expectedCount = WATCHER_COUNT;
+        EventHandler evt = (s, e) =>
+        {
+          var count = Interlocked.Increment(ref eventTriggerCount);
+          Thread.Sleep(WAIT_TIMEOUT * 2);
+        };
+        var watchers = CreateWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        FireEvents(); //Fire events again with all events blocked
+        WaitForEvents(WAIT_TIMEOUT, () => eventTriggerCount >= expectedCount);
+
+        //Assert
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+
+    [Test, Order(10)]
+    public void TestSettingChangeOldEvents()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int expectedCount = WATCHER_COUNT;
+        EventHandler evt = (s, e) =>
+        {
+          Interlocked.Increment(ref eventTriggerCount);
+        };
+        var watchers = CreateOldWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        WaitForEvents(WAIT_TIMEOUT, () => eventTriggerCount >= WATCHER_COUNT);
+
+        //Assert
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeOldWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(11)]
+    public void TestSettingChangeOldException()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int exceptionCount = 0;
+        int expectedCount = WATCHER_COUNT;
+        EventHandler evt = (s, e) =>
+        {
+          var count = Interlocked.Increment(ref eventTriggerCount);
+          if (count == 1 || count == WATCHER_COUNT / 2)
+          {
+            Interlocked.Increment(ref exceptionCount);
+            throw new Exception("Test");
+          }
+        };
+        var watchers = CreateOldWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        WaitForEvents(WAIT_TIMEOUT, () => eventTriggerCount >= WATCHER_COUNT);
+
+        //Assert
+        Assert.AreEqual(exceptionCount, 2);
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeOldWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(12)]
+    public void TestSettingChangeOldLongRunning()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int expectedCount = WATCHER_COUNT;
+        EventHandler evt = (s, e) =>
+        {
+          var count = Interlocked.Increment(ref eventTriggerCount);
+          Thread.Sleep(WAIT_TIMEOUT * 2);
+        };
+        var watchers = CreateOldWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        FireEvents(); //Fire events again with all events blocked
+        WaitForEvents(WAIT_TIMEOUT, () => eventTriggerCount >= expectedCount);
+
+        //Assert
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeOldWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+
+    [Test, Order(13)]
+    public void TestSettingChangeOldEventsMany()
+    {
+      _testSync.Wait();
+      try
+      {
+        //Arrange
+        int eventTriggerCount = 0;
+        int expectedCount = WATCHER_COUNT * MANY_FACTOR;
+        EventHandler evt = (s, e) =>
+        {
+          Interlocked.Increment(ref eventTriggerCount);
+        };
+        var watchers = CreateOldWatchers(expectedCount, evt, false);
+
+        //Act
+        FireEvents();
+        WaitForEvents(WAIT_TIMEOUT * MANY_FACTOR, () => eventTriggerCount >= WATCHER_COUNT * MANY_FACTOR);
+
+        //Assert
+        Assert.AreEqual(expectedCount, eventTriggerCount);
+
+        DisposeOldWatchers(watchers);
+      }
+      finally
+      {
+        _testSync.Release();
+      }
+    }
+  }
+}
